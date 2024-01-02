e1f24575edb7fb7e7b87ff3a8ccba44cffcd64c5
saurabhbigdatadev30 <saurabhbigdatadev30@gmail.com>
2024-01-02 17:31:51 +0530

    domain driven development first commit

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderApplicationServiceImpl.java
@@ -0,0 +1,47 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+class OrderApplicationServiceImpl implements OrderApplicationService
+{
+    private final OrderCreateCommandHandler orderCreateCommandHandler;
+    private final OrderTrackCommandHandler orderTrackCommandHandler;
+
+    public OrderApplicationServiceImpl(OrderCreateCommandHandler orderCreateCommandHandler,  OrderTrackCommandHandler orderTrackCommandHandler)
+    {
+        this.orderCreateCommandHandler = orderCreateCommandHandler;
+        this.orderTrackCommandHandler = orderTrackCommandHandler;
+    }
+
+    @Override
+    public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand)
+    {
+         log.info("Inside the createOrder of testBranch1 VERSION 1");
+        return orderCreateCommandHandler.createOrder(createOrderCommand);
+    }
+
+    @Override
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        return orderTrackCommandHandler.trackOrder(trackOrderQuery);
+    }
+
+    public void testMethodA()
+    {
+        log.info("testMethodA() in the testBranch1 only");
+    }
+
+    public void testMethodB()
+    {
+        log.info("testMethodB()  in the testBranch1 only");
+    }
+}
